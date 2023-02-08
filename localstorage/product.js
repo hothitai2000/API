e9e4f5eec093c24a2f5804e6d763ded2973b9032
hothitai2000 <hothitai02032003@gmail.com>
2023-02-08 17:57:35 +0700

    update

---
+++ localstorage/product.js
@@ -0,0 +1,52 @@
+var product = [{							
+        id: 'SP1',							
+        name: 'Áo dài thêu họa tiết 111111',							
+        img:'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 122000,							
+        },							
+        {							
+        id: 'SP2',							
+        name: 'Áo dài truyền thống',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 400000,							
+        },							
+        {							
+        id: 'SP3',							
+        name: 'Áo dài',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 350000,							
+        },							
+        {							
+        id: 'SP4',							
+        name: 'Áo dài hiện đại',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 564000,							
+        },							
+        {							
+        id: 'SP5',							
+        name: 'Áo dài hiện đại',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 654000,							
+        },							
+        {							
+        id: 'SP6',							
+        name: 'Áo dài hiện đại',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 123000,							
+        },							
+        {							
+        id: 'SP7',							
+        name: 'Áo dài hiện đại',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 345000,							
+        },							
+        {							
+        id: 'SP8',							
+        name: 'Áo dài hiện đại',							
+        img: 'https://bizweb.dktcdn.net/100/243/878/files/kocis-korea-hanbok-aodai-fashionshow-03-9766157012-1.jpg?v=1514536952493',							
+        price: 258000,							
+        },							
+        ];
+        
+        							
+	
